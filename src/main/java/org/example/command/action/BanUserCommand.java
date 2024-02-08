66f76499b8b452f1f5bbe26c3951dc1892ba9750
Loleheda <loleheda@gmail.com>
2024-02-08 22:21:57 +0300

    change if and checkstyle

--- src/main/java/org/example/command/action/BanUserCommand.java
+++ src/main/java/org/example/command/action/BanUserCommand.java
@@ -3,17 +3,28 @@ package org.example.command.action;
 import org.example.command.Command;
 import org.example.data.DataBase;
 import org.example.data.User;
+import org.example.data.UserType;
+import org.example.result.ForwardResult;
 import org.example.result.RedirectResult;
 import org.example.result.Result;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.example.Resources.COMMAND_SHOW_LOGIN_PAGE;
+import static org.example.Resources.COMMAND_SHOW_CHAT_PAGE;
 import static org.example.Resources.COMMAND_SHOW_ADMIN_PANEL_PAGE;
 
 public class BanUserCommand implements Command {
     @Override
     public Result execute(HttpServletRequest request, HttpServletResponse response) {
+        User admin = (User) request.getSession().getAttribute("user");
+        if (admin == null) {
+            return new ForwardResult(COMMAND_SHOW_LOGIN_PAGE);
+        }
+        if (!admin.getUserType().equals(UserType.ADMIN)) {
+            return new ForwardResult(COMMAND_SHOW_CHAT_PAGE);
+        }
         User user = DataBase.findUserByLogin(request.getParameter("login"));
         user.setBanned(!user.isBanned());
         return new RedirectResult(COMMAND_SHOW_ADMIN_PANEL_PAGE);
--- src/main/java/org/example/command/show/ShowAdminPanelPageCommand.java
+++ src/main/java/org/example/command/show/ShowAdminPanelPageCommand.java
@@ -13,14 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.example.Resources.*;
+import static org.example.Resources.COMMAND_SHOW_LOGIN_PAGE;
+import static org.example.Resources.COMMAND_SHOW_CHAT_PAGE;
+import static org.example.Resources.PAGE_ADMIN_PANEL;
 
 public class ShowAdminPanelPageCommand implements Command {
     @Override
     public Result execute(HttpServletRequest request, HttpServletResponse response) {
         User admin = (User) request.getSession().getAttribute("user");
-        if(admin == null) return new ForwardResult(COMMAND_SHOW_LOGIN_PAGE);
-        if(!admin.getUserType().equals(UserType.ADMIN)) return new ForwardResult(COMMAND_SHOW_CHAT_PAGE);
+        if (admin == null) {
+            return new ForwardResult(COMMAND_SHOW_LOGIN_PAGE);
+        }
+        if (!admin.getUserType().equals(UserType.ADMIN)) {
+            return new ForwardResult(COMMAND_SHOW_CHAT_PAGE);
+        }
         List<User> users = DataBase.getUsers().values().stream()
                 .filter(user -> !user.getUserType().equals(UserType.ADMIN)).collect(Collectors.toList());
         request.getSession(false).setAttribute("userAll", users);
